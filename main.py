164f3afdb6e8983e5a7cd0801a446fed35ca43de
Tony Mao <twm59@cornell.edu>
2023-12-31 16:06:14 -0500

    Fetch prizepicks data from api, sort into dataframe

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-import pandas as pandas
+import pandas as pd
 import numpy as np
+import requests
+from pandas import json_normalize
+from nba_api.stats.static import players
+
+# from nba_api.stats.static import playergamelogs, leaguegamelog
+
+
+session = requests.Session()
+response = session.get(
+    "https://partner-api.prizepicks.com/projections?single_stat=True&league_id=7&per_page=100000'",
+)
+print(response.status_code)
+
+df1 = json_normalize(response.json()["included"])
+df1 = df1[df1["type"] == "new_player"]
+
+df2 = json_normalize(response.json()["data"])
+
+data = pd.merge(
+    df1, df2, how="left", left_on=["id"], right_on=["relationships.new_player.data.id"]
+)
+
+df = data[["attributes.name", "attributes.line_score", "attributes.stat_type"]]
+prizepicksptsDf = df[df["attributes.stat_type"] == "Points"]
+print(prizepicksptsDf)
